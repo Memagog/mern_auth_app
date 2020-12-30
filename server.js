f50a5d709fa274a0cbdf29027d694959b9242f13
Memagog <55204188+Memagog@users.noreply.github.com>
2020-12-30 16:13:13 +0300

    deploy heroku app

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(
 app.use(bodyParser.json());
 const db = require("./config/key").mongoURI;
 
+
 mongoose.connect(
     db
     , {useNewUrlParser: true},
@@ -34,7 +35,17 @@ mongoose.connect(
 
 app.use(passport.initialize());
 require("./config/passport")(passport);
-app.use("/api/users",users);
+app.use("/api/users", users);
+
+if(process.env.NODE_ENV === 'production'){
+
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res)=>{
+            res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen( port, () => console.log(`Server has been started ${port}!`));
